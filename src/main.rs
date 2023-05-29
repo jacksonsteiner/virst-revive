c61ea7cc080897fde45cf45c083655421b700aa9
Jackson Steiner <jacksonsteiner@fastmail.com>
2023-05-29 19:46:23 -0400

    ensure correct number of args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-use virt::connect::Connect;
+//use std::io;
+use std::env;
+use std::process::exit;
+use quick_xml::reader::Reader;
+//use virt::connect::Connect;
+
+fn check_args() {
+
+    let argv: Vec<String> = env::args().collect();
+
+    if argv.len() != 2 {
+        eprintln!("FATAL ERROR: INCORRECT USAGE. \
+Use with one argument as absolute path to domain xml: \
+virsh-persist </absolute/path/to/domain/xml.xml>");
+        exit(exitcode::USAGE)
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+
+    check_args();
+
 }
